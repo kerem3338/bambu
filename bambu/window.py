81df83aa62d533fdc3ed741fd241256e10ca5021
kerem3338 <73388608+kerem3338@users.noreply.github.com>
2021-08-14 14:04:26 +0300

    Update window.py

--- bambu/window.py
+++ bambu/window.py
@@ -1,5 +1,10 @@
 import pygame
 import pymsgbox
+try:
+    import win32gui, win32con
+except:
+    print("Lütfen pywin32 paketini kurun (kurulu olmadan da motor çalışır ama hide_console() ögesini kullanmazsınız!")
+
 class Window:
     def __init__(self):
         pygame.init()
@@ -7,3 +12,12 @@ class Window:
 
     def alert(self, message, title):
         pymsgbox.alert(message, title)
+
+    
+    def hide_console(self):
+        #Uyarı hide console sadece kodu çalıştıran uygulamayı gizler
+        #yani vs code veya farklı bir ide den kodu çalıştırsanız ide penceresini gizler ama ide hala çalışır vaziyettedir
+        hide = win32gui.GetForegroundWindow()
+        win32gui.ShowWindow(hide , win32con.SW_HIDE)
+
+    
