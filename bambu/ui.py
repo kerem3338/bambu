efd3244f2cc4297167fd7209f214459c0aa9b775
kerem3338 <73388608+kerem3338@users.noreply.github.com>
2021-08-14 14:04:46 +0300

    Create ui.py

---
+++ bambu/ui.py
@@ -0,0 +1,25 @@
+import pygame
+import colors
+
+class Ui:
+    def __init__(self):
+        pygame.init()
+        self.col = colors.color_list
+        
+    
+    def load_image(self, screen, image, x, y):
+        image = pygame.image.load(image)
+        screen.blit(image,(x, y))
+        pygame.display.update()
+
+            
+    def draw_circle(self, screen, color, x, y, size):
+        if color in self.col:
+            pygame.draw.circle(screen, (color),(x, y), size)
+            pygame.display.update()
+        else:
+            print(f"draw_circle: {color} bulunamadı")
+            
+    def clear_screen(self, screen):
+        screen.fill((0, 0, 0))
+        pygame.display.update()
